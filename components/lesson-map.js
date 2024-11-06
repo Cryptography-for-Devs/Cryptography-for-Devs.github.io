bdb78338f81ce55aeaa1429018a97fa9fe61ad77
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2024-11-06 20:54:16 +0530

    add authors

--- components/lesson-map.js
+++ components/lesson-map.js
@@ -5,11 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
         🧭 Lesson Map
         <button id="lesson-map-close-btn">&times;</button>
       </h2>
-      <ol start="0">
-        <li><a href="${window.location.origin}/0-before-we-start">Before we start! (Do not skip this)</a></li>
 
-        <div class="separator"></div>
+      <ol start="0" class="lesson-map-ol">
 
+        <li><a href="${window.location.origin}/0-before-we-start">Before we start (Do not skip this)</a></li>
         <li><a href="${window.location.origin}/1-symmetric-key-encryption">Symmetric key encryption</a></li>
         <li><a href="${window.location.origin}/2-caesar-challenge">Code challenge: Caesar's cipher</a></li>
         <li><a href="${window.location.origin}/3-the-hash">Cryptographic hash functions</a></li>
@@ -17,22 +16,19 @@ document.addEventListener("DOMContentLoaded", () => {
         <li><a href="${window.location.origin}/5-rainbows-and-salts">Rainbow table and the salt</a></li>
         <li><a href="${window.location.origin}/6-slow-them-down">PBKDF-2: slowing down</a></li>
 
-        <p class="info-alert" id="upcoming-lessons-map-info">We are working hard on the upcoming lessons, please <a href="${window.location.origin}/our-community">join our email or slack community</a> to stay in touch with our future happenings!</p>
+        <p class="info-alert" id="upcoming-lessons-map-info">To know when the future lessons' release, please <a href="https://twitter.com/vkwebdev" target="_blank">follow me on twitter</a> -- or -- <a href="${window.location.origin}/our-community">join my rare-update email letter</a>.</p>
 
-        <div class="separator"></div>
+        <div class="lesson-map-separator"></div>
 
-        <div id="upcoming-lessons">
+        <div class="lesson-map-upcoming-lessons">
             <li>The Diffie-Hellman key exchange</li>
             <li>RSA public-key encryption</li>
             <li>Challenge: End to end encryption</li>
             <li>The TLS story</li>
             <li>Certificate authorities rule!</li>
-            <li>Project: Finale.</li>
-
-            <div class="separator"></div>
-
-            <li>The Farewell <3</li>
+            <li>Project: Finale. 💛</li>
         </div>
+      
       </ol>
     </div>
     `;
